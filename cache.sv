48e0a1fcfbc348a90aa6096f0ab353468a9f410e
mdingucsb <148507143+mdingucsb@users.noreply.github.com>
2025-05-19 14:07:48 -0700

    Update cache.sv

--- cache.sv
+++ cache.sv
@@ -9,7 +9,7 @@ module ucsbece154b_icache #(
 
   // core fetch interface
   input logic readEnable, // ~StallF
-  input logic [31:0] readAddress, // PCNewF
+  input logic [31:0] readAddress, // PC
   output logic [WORD_SIZE-1:0] instruction,
   output logic ready, // means valid instruction output, next readAddress can be input
   output logic busy, // if false, insert NO-OP instruction to fetch (addi, x0, x0, 0)
@@ -21,14 +21,15 @@ module ucsbece154b_icache #(
   output logic memReadRequest, // asserted when cache miss, lowered when data ready received (assert for roughly T0)
   input logic FlushD,
   output logic FlushD_alt,
-  output logic read_to_delay
+  output logic read_to_delay,
+  input logic [1:0] memBlockIndex
 );
 
   localparam LOG_NUM_SETS = $clog2(NUM_SETS);
-
   localparam LOG_NUM_WAYS = $clog2(NUM_WAYS);
   localparam LOG_BLOCK_WORDS = $clog2(BLOCK_WORDS);
-  integer i, j, k, l;
+
+  integer i, j, k, l, m;
 
   typedef enum logic [1:0] {read, delay, words, write} state_t;
   state_t stateNext, stateReg;
@@ -42,23 +43,21 @@ module ucsbece154b_icache #(
     block_t data; 
   } way_t;
 
-  typedef struct {
-    logic [29-LOG_NUM_SETS-LOG_BLOCK_WORDS:0] tag;
-    block_t data;
-  } buffer_t;
-
   typedef way_t set_t [0:NUM_WAYS-1]; // 4 ways in a set
   set_t SRAM [0:NUM_SETS-1]; // 8 sets in the memory
 
-  buffer_t buffer;
-
+  logic [29-LOG_BLOCK_WORDS:0] bufferTag;
+  logic [31:0] bufferData [0:BLOCK_WORDS-1];
   logic [29-LOG_NUM_SETS-LOG_BLOCK_WORDS:0] tagIndex;
   logic [LOG_NUM_SETS-1:0] setIndex;
+  logic [LOG_NUM_SETS-1:0] setIndex_w;
+  logic [LOG_NUM_SETS-1:0] setIndex_i;
   logic [LOG_BLOCK_WORDS-1:0] blockIndex;
   logic cacheHit;
   logic [31:0] DataIn;
   logic [29-LOG_NUM_SETS-LOG_BLOCK_WORDS:0] memTagIndex;
-  logic [LOG_BLOCK_WORDS-1:0] memBlockIndex;
+  logic [LOG_BLOCK_WORDS:0] num_words;
+  logic [31:0] oldAddress;
   
   logic [LOG_NUM_WAYS-1:0] hitWay;
 
@@ -72,41 +71,55 @@ module ucsbece154b_icache #(
   assign tagIndex = readAddress[31:LOG_NUM_SETS+LOG_BLOCK_WORDS+2]; // [31:7]
   assign setIndex = readAddress[LOG_NUM_SETS+LOG_BLOCK_WORDS+1:LOG_BLOCK_WORDS+2]; // [6:4]
   assign blockIndex = readAddress[LOG_BLOCK_WORDS+1:2]; // [3:2]
+  assign memTagIndex = memReadAddress[31:LOG_NUM_SETS+LOG_BLOCK_WORDS+2];
 
-  always_comb begin
+  always_ff @(posedge clk) begin
+    oldAddress <= readAddress;
+    if (stateReg == delay && stateNext == words)
+      num_words <= BLOCK_WORDS - readAddress[3:2];
+  end
+
+  always_comb begin // resume here
     // defaults
     busy = 1'b0;
     memReadAddress = readAddress;
     memReadRequest = 1'b0;
     ready = 1'b0;
-
+    // instruction = 32'h00000013;
     cacheHit = 1'b0;
     hitWay = 2'b00;
+
+    for (l = 0; l < NUM_WAYS; l++) begin
+      if (tagIndex == SRAM[setIndex][l].tag && SRAM[setIndex][l].v) begin // if match and valid
+        cacheHit = 1'b1;
+        hitWay = l;
+      end
+    end
 
     // output logic
     case (stateReg)
       read: begin
-        for (l = 0; l < NUM_WAYS; l++) begin
-          if (tagIndex == SRAM[setIndex][l].tag && SRAM[setIndex][l].v) begin // if match and valid
-            cacheHit = 1'b1;
-            hitWay = l;
-          end
-        end
         if (cacheHit) begin
           instruction = SRAM[setIndex][hitWay].data[blockIndex];
           ready = 1'b1;
         end else begin
-          instruction <= 32'h00000013;
           memReadRequest = 1'b1;
         end
       end
       delay: begin
         memReadRequest = 1'b1;
-        for (l = 0; l < NUM_WAYS; l++) begin
-          if (tagIndex == SRAM[setIndex][l].tag && SRAM[setIndex][l].v) begin // if match and valid
-            cacheHit = 1'b1;
-            hitWay = l;
-          end
+        if (cacheHit) begin
+          instruction = SRAM[setIndex][hitWay].data[blockIndex];
+          ready = 1'b1;
+        end
+      end
+      words: begin
+        if ((readAddress == oldAddress + 4) && (words_wait_counter != num_words - 1)) begin // might be num_words instead
+          instruction = memDataIn;
+          ready = 1'b1;
+        end else if (cacheHit) begin
+          instruction = SRAM[setIndex][hitWay].data[blockIndex];
+          ready = 1'b1;
         end
       end
       write: begin
@@ -133,18 +146,19 @@ module ucsbece154b_icache #(
         buffer.tag <= memTagIndex;
         buffer.data[memBlockIndex] <= memDataIn;
       end
-      // if (stateReg == write) begin // synchronous write
-      //   SRAM[setIndex][randBits].v <= 1;
-      //   SRAM[setIndex][randBits].tag <= tagIndex;
-      //   SRAM[setIndex][randBits].data[words_wait_counter] <= DataIn;
-      
-      // write
+      if (stateNext == write) begin
+        SRAM[setIndex_w][randBits].v <= 1;
+        SRAM[setIndex_w][randBits].tag <= tagIndex;
+        for (m = 0; m < BLOCK_WORDS; m++) begin
+          SRAM[setIndex_w][randBits].data[m] <= bufferData[m];
+        end
+      end
     end
   end
 
   // counter for words_wait
   always_ff @(posedge clk) begin
-    if (reset || (stateReg == delay && stateNext == write)) begin // set to 0 upon reset or transition to write
+    if (reset || (stateReg == delay && stateNext == words)) begin // set to 0 upon reset or transition to write
       words_wait_counter <= 0;
     end else begin
       if (words_wait_counter == BLOCK_WORDS - 1)
@@ -165,7 +179,7 @@ module ucsbece154b_icache #(
           else
             stateNext = delay;
         end else
-          stateNext = delay;
+          stateNext = delay; // why is this here? possible candidate for removal
       end
       delay: begin
         if (memDataReady)
@@ -189,6 +203,8 @@ module ucsbece154b_icache #(
       stateReg <= read;
     end else begin
       stateReg <= stateNext;
+      setIndex_w <= setIndex_i;
+      setIndex_i <= setIndex;
     end
   end
 
